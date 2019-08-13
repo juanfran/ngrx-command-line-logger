8c398326815c01893bfd6a8a396ff673788fabe5
Juanfran <juanfran.ag@gmail.com>
2019-08-13 18:23:27 +0200

    feat(back): repl(.old, .new)

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import WebSocket from 'ws';
 import chalk from 'chalk';
+import { diffString } from 'json-diff';
+import repl from 'repl';
+import util from 'util';
 
 const args = process.argv.slice(2).reduce((prev, current, index, list) => {
   if (current.startsWith('--') && list[index + 1]) {
@@ -21,7 +24,13 @@ const wss = new WebSocket.Server({
 });
 
 function deepLog(obj: {}) {
-  console.dir(obj, {depth: null, colors: true})
+  console.log(util.inspect(obj, {
+    showHidden: false,
+    depth: null,
+    colors: true,
+    compact: false
+  }));
+  // console.log(JSON.stringify(obj, null, 2));
 }
 
 function breakLine(lines = 1) {
@@ -29,6 +38,12 @@ function breakLine(lines = 1) {
     console.log('');
   }
 }
+
+let lastMessage:any = {
+  action: null,
+  state: null,
+  newState: null
+};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message: string) {
@@ -38,17 +53,61 @@ wss.on('connection', function connection(ws) {
       newState: any
     } = JSON.parse(message);
 
+    breakLine();
     console.log(chalk.bold.bgGreen(' Action '), ' ', new Date());
     deepLog(obj.action);
 
     breakLine();
-    console.log(chalk.bgHex('#FF8800').bold(' Old state '));
-    deepLog(obj.state);
 
-    breakLine();
-    console.log(chalk.bold.bgCyan(' New state '));
-    deepLog(obj.newState);
+    console.log(chalk.bgHex('#FF8800').bold(' Diff '));
+    console.log(diffString(obj.state, obj.newState));
 
     breakLine(2);
+
+    replServer.displayPrompt();
+
+    lastMessage = obj;
   });
+});
+
+const replServer = repl.start({
+  completer: (line: any) => {
+    const completions = [
+      '.old',
+      '.new',
+      '.exit'
+      // dispatch() TODO
+    ];
+
+    const hits = completions.filter(function(c) { return c.indexOf(line) == 0 });
+
+    return [hits.length ? hits : completions, line]
+  }
+});
+
+replServer.defineCommand('old', {
+  help: 'Old state',
+  action() {
+    this.clearBufferedCommand();
+    breakLine();
+    deepLog(lastMessage.state);
+    breakLine();
+    this.displayPrompt();
+  }
+});
+
+replServer.defineCommand('new', {
+  help: 'New state',
+  action() {
+    this.clearBufferedCommand();
+    breakLine();
+    deepLog(lastMessage.newState);
+    breakLine();
+    this.displayPrompt();
+  }
+});
+
+replServer.on('exit', () => {
+  wss.close();
+  process.exit();
 });
